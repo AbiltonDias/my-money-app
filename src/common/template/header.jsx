3182601e0dc9b0914e0b8fb9ac228a82435d7a86
Abilton Dias <abiltongd@gmail.com>
2018-02-28 17:42:13 -0300

    Carraga inicial do meu sistema

---
+++ src/common/template/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default props => (
+    <header className='main-header'>
+        <a href='#' className='logo'>
+            <span className='logo-mini'><b>My</b>M</span>
+            <span className='logo-log'>
+                <i className='fa fa-money'></i>
+                <b>My</b> Money
+             </span>
+        </a>
+        <nav className='navbar navbar-static-top'>
+            <a href className='sidebar-toggle' data-togger='offcanvas'></a>
+        </nav>
+     </header>
+
+
+)
